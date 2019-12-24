7698b7b20e2bb7ef637a341de17627770ecf861b
Andrew Capp <acapp909@gmail.com>
2019-12-24 03:06:25 +0000

    Automatically backed up by Learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,16 +1,4 @@
 # Create your HauntedHouse class here
 
 class HauntedHouse < ActiveRecord::Base
-  def change
-    create_table :haunted_houses do |t|
-      t.string :name
-      t.string :location
-      t.string :theme
-      t.string :price
-      t.boolean :family_friendly
-      t.string :opening_date
-      t.string :closing_date
-      t.string :long_description
-    end
-  end
 end
